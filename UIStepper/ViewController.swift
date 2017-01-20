722b0697372148bdcb7b0e0ee1439b2d0bbc8d4e
Shrawan Shinde <shrawanshinde@Shrawans-MacBook-Pro.local>
2017-01-21 01:44:34 +0530

    A stepper control provides a user interface for incrementing or decrementing a value. A stepper displays two buttons, one with a minus (“–”) symbol and one with a plus (“+”) symbol.

--- UIStepper/ViewController.swift
+++ UIStepper/ViewController.swift
@@ -10,9 +10,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var stepperValue = UILabel()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        stepperValue.frame = CGRect(x: 100, y: 200, width: 200, height: 20)
+        view.addSubview(stepperValue)
+        stepperValue.text = "Value of UIStepper : 0"
+
+        
+        var stepperView = UIStepper (frame:CGRect(x: 110, y: 250, width: 0, height: 0))
+        stepperView.wraps = true
+        stepperView.autorepeat = true
+        stepperView.maximumValue = 10
+        stepperView.addTarget(self, action: #selector(stepperValueChanged), for: UIControlEvents.valueChanged)
+        self.view.addSubview(stepperView)
+        
+    }
+    
+    func stepperValueChanged(sender: UIStepper) {
+        stepperValue.text = "Value of UIStepper : "+Int(sender.value).description
     }
 
     override func didReceiveMemoryWarning() {
